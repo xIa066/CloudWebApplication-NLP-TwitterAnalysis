04cb9a5f97e98f21a921dbca35a018ec3df0c7a7
Alice Chen <zixic@student.unimelb.edu.au>
2021-05-18 23:49:46 +1000

    Update app.py

--- API/app.py
+++ API/app.py
@@ -90,7 +90,16 @@ def getLocation():
         data = get_view('location',db,False)
     return data
 
-
+@app.route('/textdoc',methods=['GET'])
+def getLocation():
+    try:
+        data = get_view('textdoc',db,False)
+    except:
+        server = connect_server()
+        get_db(DATABASE_HISTORY, server)
+        data = get_view('textdoc',db,False)
+    return data
+    
 if __name__ == '__main__':
     #app.run(debug=True)                       # debug mode
     app.run(host='0.0.0.0', port=8000)        # deployment mode
